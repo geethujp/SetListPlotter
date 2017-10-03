14628fb92821170bb275842d8e2a3eac5b456ce3
geethu <geethu@QB-TP3-1042.local>
2017-10-03 18:31:02 +0530

    Created components for header, footer and list. Wrote service to load data from json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import setInfo from '../assets/json/setInfo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'Set List Plotter';
-  data = setInfo
 }
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'setlist-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss'],
+})
+
+export class FooterComponent {
+    title = 'Footer'
+}
